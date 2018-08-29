4b08311490dfca467eb503bb12fdab92f38a676c
HELEN-LAPTOP\Helen <helebeen@mail.upb.de>
2018-08-29 17:13:39 +0200

    implement the Kendell results

--- JAICore/jaicore-modifiedISAC/src/main/java/jaicore/modifiedISAC/evalutation/modifiedISACEvaluator.java
+++ JAICore/jaicore-modifiedISAC/src/main/java/jaicore/modifiedISAC/evalutation/modifiedISACEvaluator.java
@@ -1,6 +1,12 @@
 package jaicore.modifiedISAC.evalutation;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.commons.math3.stat.correlation.KendallsCorrelation;
+
 import jaicore.CustomDataTypes.RankingForGroup;
+import jaicore.CustomDataTypes.Solution;
 import jaicore.modifiedISAC.ModifiedISAC;
 import jaicore.modifiedISAC.ModifiedISACInstanceCollector;
 import weka.core.Instance;
@@ -11,6 +17,7 @@ public class modifiedISACEvaluator {
 		double[] results = new double[data.numInstances()];
 		
 		for (int i = 0; i < data.numInstances(); i++) {
+			HashMap<String,Integer> positionInRanking = new HashMap<String,Integer>();
 			Instances trainingData = new Instances(data);
 			Instance testInstance = trainingData.get(i);
 			trainingData.delete(i);
@@ -20,13 +27,46 @@ public class modifiedISACEvaluator {
 			isac.bulidRanker();
 			RankingForGroup<double[], String> ranking = isac.getRanking(testInstance);
 			// get the ground truth ranking as string list
-			
+			HashMap<String,Double> ClassandPerfo = new HashMap<String,Double>();
+			for(int j = 1; j<104;j++) {
+				ClassandPerfo.put(testInstance.attribute(j).name(),testInstance.value(j));
+			}
+			double[] rankingTruth = new double[21];
+			int tmp = 0; 
+		
+			while(!ClassandPerfo.isEmpty()) {
+				double maxPerfo = Double.MAX_VALUE;
+				String myClassi = null;
+				for(String classi: ClassandPerfo.keySet()) {
+					if(!ClassandPerfo.get(classi).isNaN()) {
+						if(ClassandPerfo.get(classi)>=maxPerfo) {
+							maxPerfo = ClassandPerfo.get(classi);
+							myClassi = classi;
+							
+						}
+					}
+				}
+				rankingTruth[tmp] = tmp;
+				positionInRanking.put(myClassi,tmp);
+				ClassandPerfo.remove(myClassi);
+				tmp++;
+			}
 			// get the ranking as string list
-			
+				ArrayList<String> rankingAsStringList = new ArrayList<String>();
+				for(Solution<String> rank: ranking.getRanking()) {
+					rankingAsStringList.add(rank.getSolution());
+				}
+				double[] rankingFromMyMethod = new double[21];
+				int intermidiate = 0;
+				for(String classi : rankingAsStringList) {
+					rankingFromMyMethod[intermidiate] = positionInRanking.get(classi);
+				}
+				KendallsCorrelation test= new KendallsCorrelation();
+				double thisResult = test.correlation(rankingTruth, rankingFromMyMethod);
 			// double value as result of this evaluation
-			double thisResult=0;
+
 			results[i]=thisResult;
 		}
-		return null;
+		return results;
 	}
 }
